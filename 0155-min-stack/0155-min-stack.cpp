d3400de708e06aac3963759194ab30e4265fd96b
Jyoti <151373033+JotiThakur88@users.noreply.github.com>
2026-04-22 11:12:33 +0530

    Time: 4 ms (43.94%), Space: 23.3 MB (66.67%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,27 @@
+#include <stack>
+using namespace std;
+
+class MinStack {
+public:
+    stack<pair<int,int>> st;
+
+    void push(int val) {
+        if (st.empty()) {
+            st.push({val, val});
+        } else {
+            st.push({val, min(val, st.top().second)});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
